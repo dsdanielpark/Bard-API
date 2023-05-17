68936e346d8e1e065796abc2f6a50223f6008a8a
amit9021 <amit9021@gmail.com>
2023-05-17 13:03:52 +0300

    change run

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 from bardapi.core import Bard
 import os
 
-# Close the browser
-
-
-Bard().get_answer("how are you?")['content']
 print(Bard().get_answer("how are you?")['content'])
 
 print("Done")
