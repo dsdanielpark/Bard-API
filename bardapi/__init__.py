4d9e72b690d831e399a4ac5031601e579e19f749
parkminwoo <parkminwoo1991@gmail.com>
2023-05-18 15:26:21 +0900

    fix: change variable name

--- bardapi/__init__.py
+++ bardapi/__init__.py
@@ -1,8 +1,8 @@
 import os
 from bardapi.core import Bard
 
-api_key = os.environ.get("_BARD_API_KEY")
+bard_api_key = os.environ.get("_BARD_API_KEY")
 
-__all__ = ["Bard", "api_key"]
-__version__ = "0.1.5"
+__all__ = ["Bard", "bard_api_key"]
+__version__ = "0.1.6"
 __author__ = "daniel park <parkminwoo1991@gmail.com>"
