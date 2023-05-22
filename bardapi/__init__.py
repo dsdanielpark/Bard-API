091ccd83011f222b61f78ee00e0f4693620bd7dd
parkminwoo <parkminwoo1991@gmail.com>
2023-05-22 16:33:21 +0900

    build: 0.1.11 clean bardapi

--- bardapi/__init__.py
+++ bardapi/__init__.py
@@ -1,9 +1,8 @@
 import os
 from bardapi.core import Bard
-from bardapi.tranlsator import translate
 
 bard_api_key = os.environ.get("_BARD_API_KEY")
 
-__all__ = ["Bard", "bard_api_key", "translate"]
-__version__ = "0.1.10"
+__all__ = ["Bard", "bard_api_key"]
+__version__ = "0.1.11"
 __author__ = "daniel park <parkminwoo1991@gmail.com>"
