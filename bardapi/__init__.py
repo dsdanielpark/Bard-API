0059c5e8070f2f684780abfbbfb94f7dc24443e3
parkminwoo <parkminwoo1991@gmail.com>
2023-05-26 22:32:16 +0900

    feat: add chatbard

--- bardapi/__init__.py
+++ bardapi/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from bardapi.constants import ALLOWED_LANGUAGES, DEFAULT_LANGUAGE, SEPARATOR_LINE, SESSION_HEADERS, USER_PROMPT
 from bardapi.core import Bard
+from bardapi.chat import ChatBard
 
 
 bard_api_key = os.environ.get("_BARD_API_KEY")
@@ -14,7 +15,8 @@ __all__ = [
     "ALLOWED_LANGUAGES",
     "DEFAULT_LANGUAGE",
     "SEPARATOR_LINE",
-    "USER_PROMPT",
+    "USER_PROMPT", 
+    "ChatBard"
 ]
 __version__ = "0.1.11"
 __author__ = "daniel park <parkminwoo1991@gmail.com>"
