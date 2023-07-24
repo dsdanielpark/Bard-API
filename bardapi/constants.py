e71a3b5c41893192f7bf7cbf46602b851c54d98b
Minwoo(Daniel) Park <parkminwoo1991@gmail.com>
2023-07-25 01:30:07 +0900

    chore: note in constants.py

--- bardapi/constants.py
+++ bardapi/constants.py
@@ -18,10 +18,10 @@ IMG_UPLOAD_HEADERS = {
     "authority": "content-push.googleapis.com",
     "accept": "*/*",
     "accept-language": "en-US,en;q=0.7",
-    "authorization": "Basic c2F2ZXM6cyNMdGhlNmxzd2F2b0RsN3J1d1U=",  # constant authorization key
+    "authorization": "Basic c2F2ZXM6cyNMdGhlNmxzd2F2b0RsN3J1d1U=",  # Constant Authorization Key
     "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
     "origin": "https://bard.google.com",
-    "push-id": "feeds/mcudyrk2a4khkz",  # constant
+    "push-id": "feeds/mcudyrk2a4khkz",                              # Constant
     "referer": "https://bard.google.com/",
     "x-goog-upload-command": "start",
     "x-goog-upload-header-content-length": "",
