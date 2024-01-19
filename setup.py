0c9c60c39a4efcb93d5c9fa97361b7aef82e2715
dsdanielpark <parkminwoo1991@gmail.com>
2024-01-20 02:08:25 +0900

    del: python 3.6 due to type hint and other higher python features

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ setup(
         "Intended Audience :: Science/Research",
         "Natural Language :: English",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
