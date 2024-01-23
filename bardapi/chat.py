efabce8720607b88174dc3f84f27520edff6cf8b
dsdanielpark <parkminwoo1991@gmail.com>
2024-01-23 19:54:15 +0900

    fix: clean import

--- bardapi/chat.py
+++ bardapi/chat.py
@@ -1,12 +1,10 @@
 import os
 import requests
 from typing import Optional
-from bardapi import Bard
 from colorama import Fore, Back, Style
+from bardapi.core import Bard
 from bardapi.constants import SEPARATOR_LINE, SESSION_HEADERS
-from bardapi.utils import (
-    extract_bard_cookie,
-)
+from bardapi.utils import extract_bard_cookie
 
 SESSION_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
