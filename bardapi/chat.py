3eab54f9e3362fd410da576fd9424821bfd720d4
dsdanielpark <parkminwoo1991@gmail.com>
2024-01-23 20:15:26 +0900

    chore: delete session header val

--- bardapi/chat.py
+++ bardapi/chat.py
@@ -6,10 +6,6 @@ from bardapi.core import Bard
 from bardapi.constants import SEPARATOR_LINE, SESSION_HEADERS
 from bardapi.utils import extract_bard_cookie
 
-SESSION_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-}
-
 
 class ChatBard(Bard):
     """
