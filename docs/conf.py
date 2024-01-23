6149e5cd0f85ae090d5509f9cdd5034e02ddd4e9
dsdanielpark <parkminwoo1991@gmail.com>
2024-01-23 19:58:02 +0900

    chore: style black

--- docs/conf.py
+++ docs/conf.py
@@ -12,21 +12,22 @@
 #
 import os
 import sys
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('.'))
 
 # This will include the necessary source files folders in the PATH to be able to generate the documentation from.
 # devdir=r'C:\Users\parkm\Desktop\git\ExceptionNotifier'
 # try:
 #     if os.environ['DEVDIR']:
-#         devdir = os.environ['DEVDIR'] 
+#         devdir = os.environ['DEVDIR']
 # except KeyError:
 #     print('Unable to obtain $DEVDIR from the environment.')
 #     pass
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath("../.."))
 sys.setrecursionlimit(1500)
 
 
@@ -36,14 +37,15 @@ def skip(app, what, name, obj, skip, options):
         return False
     return skip
 
+
 # -- Project information -----------------------------------------------------
 
-project = 'bardapi'
-copyright = '2024, minwoo park'
-author = 'minwoo park'
+project = "bardapi"
+copyright = "2024, minwoo park"
+author = "minwoo park"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.39'
+release = "0.1.39"
 
 
 # -- General configuration ---------------------------------------------------
@@ -51,12 +53,10 @@ release = '0.1.39'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-   'sphinx.ext.autodoc','sphinx.ext.todo'
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 todo_include_todos = True
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -69,9 +69,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
